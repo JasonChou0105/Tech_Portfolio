1a30146b04a985cf801fdce85df7f5b80dfdbfc0
Jason Chou <JasonChou0105@gmail.com>
2024-05-20 21:58:50 -0400

    tentitive finish on process container

--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -37,10 +37,30 @@ function Project({ title, subheader, images, description }) {
           </div>
         </div>
       </div>
-      <ShowWhenInViewport className="process-container">
+      <div className="process-container">
         <div className="spacer seperator" />
-        <div className="process-header sub-header-container">Process</div>
-      </ShowWhenInViewport>
+        <ShowWhenInViewport className="process-inner-container">
+          <div className="process-content-container">
+            <div className="process-header sub-header-container">Process</div>
+            <div className="process-description-container">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+              molestias temporibus laborum, sapiente incidunt voluptatem
+              accusamus molestiae aut quia in dignissimos vel pariatur dolorem.
+              Iure eius corporis alias tempore nihil.
+            </div>
+          </div>
+          <div className="process-image-container">
+            <div className="process-image-header sub-header-container">
+              Idek what to do
+            </div>
+            <img
+              className="process-image"
+              src="/images/dream-space/House.jpeg"
+              alt=""
+            />
+          </div>
+        </ShowWhenInViewport>
+      </div>
     </div>
   );
 }
